c9efd6b63acd83201e7a8ef21f5cd9b9c6cc3e2c
Mike Hayford <mjhoptics@gmail.com>
2019-05-26 09:29:05 -0700

    rename tuple to IdealImager. bulletproof ideal_imager_setup and use keyword args for input.

--- src/rayoptics/optical/specsheet.py
+++ src/rayoptics/optical/specsheet.py
@@ -8,13 +8,13 @@
 .. codeauthor: Michael J. Hayford
 """
 
-from collections import namedtuple
 import math
+from collections import namedtuple
 
 from rayoptics.optical.model_enums import (PupilType, FieldType)
 
-FiniteConj = namedtuple('FiniteConj', ['m', 's', 'sp', 'tt', 'f'])
-""" tuple grouping together first order finite conjugate specifications
+IdealImager = namedtuple('IdealImager', ['m', 's', 'sp', 'tt', 'f'])
+""" tuple grouping together first order specifications
 
     Attributes:
         m: (lateral) magnification
@@ -24,13 +24,119 @@ FiniteConj = namedtuple('FiniteConj', ['m', 's', 'sp', 'tt', 'f'])
         f: focal length
 """
 
-InfiniteConj = namedtuple('InfiniteConj', ['m', 's', 'sp', 'tt', 'f'])
-""" tuple grouping together first order infinite conjugate specifications
 
-    Attributes:
+def ideal_imager_setup(**kwargs):
+    """ Calculate the ideal imaging properties given two independent parameters
+
+    Given 2 system parameters from the following list, this function
+    calculates the remaining parameters.
+
+    Note that if specifying ``tt`` and ``f``, their ratio, tt/f, must be
+    greater than or equal to 4. A `ValueError` is raised otherwise.
+
+    For a typical system, the value of ``s`` is negative, i.e. the object is to
+    the left of the first principal plane.
+
+    Example::
+
+        In [3]: m1s1 = first_order_setup(m=-0.5, s=-10.0); m1s1
+        Out[3]: IdealImager(m=-0.5, s=-10.0, sp=5.0, tt=15.0, f=3.333333333333)
+
+        In [4]: s_inf_efl = first_order_setup(s=-math.inf, f=25.0); s_inf_efl
+        Out[4]: IdealImager(m=-0.0, s=-inf, sp=25.0, tt=inf, f=25.0)
+
+    Args:
+        m: (lateral) magnification
+        s: object distance from first principal plane, P1->Obj
         sp: image distance from second principal plane, P2->Img
+        tt: total track length, tt = sp - s
         f: focal length
-"""
+
+    Returns:
+        :class:`IdealImager` namedtuple
+
+    Raises:
+        ValueError: if tt/f < 4
+    """
+    if 'm' in kwargs:
+        m = kwargs['m']
+        if 's' in kwargs:
+            s = kwargs['s']
+            sp = m*s
+            tt = sp - s
+            f = s*sp/(s - sp)
+        elif 'sp' in kwargs:
+            sp = kwargs['sp']
+            s = sp/m
+            tt = sp - s
+            f = s*sp/(s - sp)
+        elif 'tt' in kwargs:
+            tt = kwargs['tt']
+            s = tt/(m - 1)
+            sp = m*s
+            f = s*sp/(s - sp)
+        elif 'f' in kwargs:
+            f = kwargs['f']
+            tt = -f*(m - 1)**2/m
+            s = tt/(m - 1)
+            sp = m*s
+        else:
+            return None
+
+    elif 's' in kwargs:
+        # arrange calculations so that s=-inf is handled gracefully
+        s = kwargs['s']
+        if 'sp' in kwargs:
+            sp = kwargs['sp']
+            f = 1/(1/sp - 1/s)
+            m = sp/s
+            tt = sp - s
+        elif 'tt' in kwargs:
+            tt = kwargs['tt']
+            m = 1 + tt/s
+            sp = m*s
+            f = s*sp/(s - sp)
+        elif 'f' in kwargs:
+            f = kwargs['f']
+            m = f/(s + f)
+            sp = 1/(1/f + 1/s)
+            tt = sp - s
+        else:
+            return None
+
+    elif 'sp' in kwargs:
+        # arrange calculations so that sp=inf is handled gracefully
+        sp = kwargs['sp']
+        if 'tt' in kwargs:
+            tt = kwargs['tt']
+            m = sp/(sp - tt)
+            s = sp/m
+            f = s*sp/(s - sp)
+        elif 'f' in kwargs:
+            f = kwargs['f']
+            m = (f - sp)/f
+            s = 1/(1/sp - 1/f)
+            tt = sp - s
+        else:
+            return None
+
+    elif 'tt' in kwargs:
+        tt = kwargs['tt']
+        if 'f' in kwargs:
+            f = kwargs['f']
+            ttf = tt/f
+            # tt/f >= 4, else no solution
+            # pick root (+) that gives |s|>=|sp|, i.e. -1 <= m < 0
+            m = ((2 - ttf) + math.sqrt(ttf*(ttf - 4)))/2
+            s = tt/(m - 1)
+            sp = m*s
+        else:
+            return None
+
+    else:
+        return None
+
+    return IdealImager(m, s, sp, tt, f)
 
 
 def na2slp(na, n=1.0):
@@ -50,13 +156,6 @@ def ang2slp(ang):
 
 class SpecSheet:
     """ Top level description of the optical model
-
-    Lightweight manager class to manage connections between a model and
-    a ui view.
-
-    The main function of AppManager is refresh_gui(). This is called after
-    a user input to the gui to update the model and call a refresh function
-    for each ui view of that model.
 
     Attributes:
         conjugates: whether the system works at finite or infinite conjugates,
@@ -165,39 +264,38 @@ class SpecSheet:
                 mag = (nao/na)*(n_k/n_0)
         return mag
 
-    def get_aperture_spec_from_mag(fconj, aper1, aper2):
+    def get_aperture_spec_from_mag(imager, aper_in, aper_out):
         n_0 = 1.0
         n_k = 1.0
-        if aper1[0] == PupilType.EPD:
-            epd = aper1[1]
-            if aper2[0] == PupilType.FNO:
-                fno = efl/epd
+        if aper_in[0] == PupilType.EPD:
+            epd = aper_in[1]
+            if aper_out == PupilType.FNO:
+                fno = imager.f/epd
                 aper = (PupilType.FNO, fno)
-            if aper2[0] == PupilType.NA:
-                slpk = -(epd/2.0)/efl
+            if aper_out == PupilType.NA:
+                slpk = -(epd/2.0)/imager.f
                 na = slp2na(slpk, n=n_k)
                 aper = (PupilType.NA, na)
-        if aper1[0] == PupilType.NAO:
-            nao = aper1[1]
+        if aper_in[0] == PupilType.NAO:
+            nao = aper_in[1]
             slp0 = na2slp(nao, n=n_0)
-            if aper2[0] == PupilType.FNO:
-                efl = epd * fno
-                slpk = mag / slp0
+            if aper_out == PupilType.FNO:
+                slpk = imager.m / slp0
                 fno = -1/(2.0*slpk)
-            if aper2[0] == PupilType.NA:
-                na = fconj.m * nao
-                slpk = na2slp(na, n=n_k)
-                efl = (epd/2.0)/slpk
-        if aper1[0] == PupilType.FNO:
-            fno = aper1[1]
-            if aper2[0] == PupilType.EPD:
-                epd = efl/fno
+                aper = (PupilType.FNO, fno)
+            if aper_out == PupilType.NA:
+                na = imager.m * nao
+                aper = (PupilType.NA, na)
+        if aper_in[0] == PupilType.FNO:
+            fno = aper_in[1]
+            if aper_out == PupilType.EPD:
+                epd = imager.f/fno
                 aper = (PupilType.EPD, epd)
-        if aper1[0] == PupilType.NA:
-            na = aper1[1]
-            slpk = n_k*math.tan(math.asin(na/n_k))
-            if aper2[0] == PupilType.EPD:
-                epd = 2*slpk*efl
+        if aper_in[0] == PupilType.NA:
+            na = aper_in[1]
+            slpk = na2slp(na, n=n_k)
+            if aper_out == PupilType.EPD:
+                epd = 2*slpk*imager.f
                 aper = (PupilType.EPD, epd)
         return aper
 
@@ -262,115 +360,6 @@ class SpecSheet:
                 mag = obj_slp/img_slp
         return mag
 
-
-def do_finite_setup(arg1, arg2):
-    """ Calculate the first order system parameters for finite conjugates
-
-    Given 2 system parameters from the following list, this function
-    calculates the remaining parameters.
-
-    Note that if specifying ``tt`` and ``f``, their ratio, tt/f, must be
-    greater than or equal to 4. A `ValueError` is raised otherwise.
-
-    For a typical system, the value of ``s`` is negative, i.e. the object is to
-    the left of the first principal plane.
-
-    First order parameters:
-        - **m**: (lateral) magnification
-        - **s**: object distance from first principal plane, P1->Obj
-        - **sp**: image distance from second principal plane, P2->Img
-        - **tt**: total track length, tt = sp - s
-        - **f**: focal length
-
-    Example::
-
-        In [1]: m1 = ('m', -0.5)
-        In [2]: s1 = ('s', -10.)
-        In [3]: m1s1 = do_finite_setup(m1, s1); m1s1
-        Out[3]: FiniteConj(m=-0.5, s=-10.0, sp=5.0, tt=15.0, f=3.333333333333)
-
-    Args:
-        arg1: first tuple of parameter key and value
-        arg2: secondtuple of parameter key and value
-
-    Returns:
-        :class:`FiniteConj` namedtuple
-
-    Raises:
-        ValueError: if tt/f < 4
-
-    """
-    finite_parms = ['m', 's', 'sp', 'tt', 'f']
-    if finite_parms.index(arg1[0]) > finite_parms.index(arg2[0]):
-        arg1, arg2 = arg2, arg1
-
-    if arg1[0] == 'm':
-        m = arg1[1]
-        if arg2[0] == 's':
-            s = arg2[1]
-            sp = m*s
-            tt = sp - s
-            f = s*sp/(s - sp)
-        elif arg2[0] == 'sp':
-            sp = arg2[1]
-            s = sp/m
-            tt = sp - s
-            f = s*sp/(s - sp)
-        elif arg2[0] == 'tt':
-            tt = arg2[1]
-            s = tt/(m - 1)
-            sp = m*s
-            f = s*sp/(s - sp)
-        else:  # arg2[0] == 'f':
-            f = arg2[1]
-            tt = -f*(m - 1)**2/m
-            s = tt/(m - 1)
-            sp = m*s
-
-    elif arg1[0] == 's':
-        # arrange calculations so that s=-inf is handled gracefully
-        s = arg1[1]
-        if arg2[0] == 'sp':
-            sp = arg2[1]
-            f = 1/(1/sp - 1/s)
-            m = sp/s
-            tt = sp - s
-        elif arg2[0] == 'tt':
-            tt = arg2[1]
-            m = 1 + tt/s
-            sp = m*s
-            f = s*sp/(s - sp)
-        elif arg2[0] == 'f':
-            f = arg2[1]
-            m = f/(s + f)
-            sp = 1/(1/f + 1/s)
-            tt = sp - s
-
-    elif arg1[0] == 'sp':
-        # arrange calculations so that sp=inf is handled gracefully
-        sp = arg1[1]
-        if arg2[0] == 'tt':
-            tt = arg2[1]
-            m = sp/(sp - tt)
-            s = sp/m
-            f = s*sp/(s - sp)
-        elif arg2[0] == 'f':
-            f = arg2[1]
-            m = (f - sp)/f
-            s = 1/(1/sp - 1/f)
-            tt = sp - s
-    elif arg1[0] == 'tt':
-        tt = arg1[1]
-        if arg2[0] == 'f':
-            f = arg2[1]
-            ttf = tt/f
-            # tt/f >= 4, else no solution
-            # pick root (+) that gives |s|>=|sp|, i.e. -1 <= m < 0
-            m = ((2 - ttf) + math.sqrt(ttf*(ttf - 4)))/2
-            s = tt/(m - 1)
-            sp = m*s
-
-    return FiniteConj(m, s, sp, tt, f)
 
 dispatch = {
   (PupilType.EPD, PupilType.FNO): SpecSheet.do_aperture_spec_to_efl,
