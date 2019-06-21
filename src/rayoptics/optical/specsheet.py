3e68b9b4d2155b0e502a457ebea257f36103d947
Mike Hayford <mjhoptics@gmail.com>
2019-06-21 15:03:36 -0700

    first version of dynamic ui for setting up imager parameters

--- src/rayoptics/optical/specsheet.py
+++ src/rayoptics/optical/specsheet.py
@@ -14,7 +14,9 @@ from collections import namedtuple
 
 from rayoptics.optical.model_enums import (PupilType, FieldType)
 
-IdealImager = namedtuple('IdealImager', ['m', 's', 'sp', 'tt', 'f'])
+ideal_imager_keys = ['m', 's', 'sp', 'tt', 'f']
+ideal_imager_labels = ["m", "s", "s'", "tt", "f"]
+IdealImager = namedtuple('IdealImager', ideal_imager_keys)
 """ tuple grouping together first order specifications
 
     Attributes:
@@ -26,7 +28,7 @@ IdealImager = namedtuple('IdealImager', ['m', 's', 'sp', 'tt', 'f'])
 """
 
 
-def ideal_imager_setup(**kwargs):
+def ideal_imager_setup(**inputs):
     """ Calculate the ideal imaging properties given two independent parameters
 
     Given 2 system parameters from the following list, this function
@@ -59,72 +61,72 @@ def ideal_imager_setup(**kwargs):
     Raises:
         ValueError: if tt/f < 4
     """
-    if 'm' in kwargs:
-        m = kwargs['m']
-        if 's' in kwargs:
-            s = kwargs['s']
+    if 'm' in inputs:
+        m = inputs['m']
+        if 's' in inputs:
+            s = inputs['s']
             sp = m*s
             tt = sp - s
             f = s*sp/(s - sp)
-        elif 'sp' in kwargs:
-            sp = kwargs['sp']
+        elif 'sp' in inputs:
+            sp = inputs['sp']
             s = sp/m
             tt = sp - s
             f = s*sp/(s - sp)
-        elif 'tt' in kwargs:
-            tt = kwargs['tt']
+        elif 'tt' in inputs:
+            tt = inputs['tt']
             s = tt/(m - 1)
             sp = m*s
             f = s*sp/(s - sp)
-        elif 'f' in kwargs:
-            f = kwargs['f']
+        elif 'f' in inputs:
+            f = inputs['f']
             tt = -f*(m - 1)**2/m
             s = tt/(m - 1)
             sp = m*s
         else:
             return None
 
-    elif 's' in kwargs:
+    elif 's' in inputs:
         # arrange calculations so that s=-inf is handled gracefully
-        s = kwargs['s']
-        if 'sp' in kwargs:
-            sp = kwargs['sp']
+        s = inputs['s']
+        if 'sp' in inputs:
+            sp = inputs['sp']
             f = 1/(1/sp - 1/s)
             m = sp/s
             tt = sp - s
-        elif 'tt' in kwargs:
-            tt = kwargs['tt']
+        elif 'tt' in inputs:
+            tt = inputs['tt']
             m = 1 + tt/s
             sp = m*s
             f = s*sp/(s - sp)
-        elif 'f' in kwargs:
-            f = kwargs['f']
+        elif 'f' in inputs:
+            f = inputs['f']
             m = f/(s + f)
             sp = 1/(1/f + 1/s)
             tt = sp - s
         else:
             return None
 
-    elif 'sp' in kwargs:
+    elif 'sp' in inputs:
         # arrange calculations so that sp=inf is handled gracefully
-        sp = kwargs['sp']
-        if 'tt' in kwargs:
-            tt = kwargs['tt']
+        sp = inputs['sp']
+        if 'tt' in inputs:
+            tt = inputs['tt']
             m = sp/(sp - tt)
             s = sp/m
             f = s*sp/(s - sp)
-        elif 'f' in kwargs:
-            f = kwargs['f']
+        elif 'f' in inputs:
+            f = inputs['f']
             m = (f - sp)/f
             s = 1/(1/sp - 1/f)
             tt = sp - s
         else:
             return None
 
-    elif 'tt' in kwargs:
-        tt = kwargs['tt']
-        if 'f' in kwargs:
-            f = kwargs['f']
+    elif 'tt' in inputs:
+        tt = inputs['tt']
+        if 'f' in inputs:
+            f = inputs['f']
             ttf = tt/f
             # tt/f >= 4, else no solution
             # pick root (+) that gives |s|>=|sp|, i.e. -1 <= m < 0
@@ -154,7 +156,7 @@ def ang2slp(ang):
     """ convert an angle in degrees to a slope """
     return math.tan(math.radians(ang))
 
-
+    
 class SpecSheet:
     """ Top level description of the optical model
 
@@ -203,18 +205,43 @@ class SpecSheet:
     
             pass
 
-    def do_aperture_spec_to_efl(aper1, aper2):
-        n_0 = 1.0
-        n_k = 1.0
-        if aper1[0] == PupilType.EPD:
-            epd = aper1[1]
-            if aper2[0] == PupilType.FNO:
-                fno = aper2[1]
-                efl = epd * fno
-            if aper2[0] == PupilType.NA:
-                na = aper2[1]
-                slpk = na2slp(na, n=n_k)
-                efl = (epd/2.0)/slpk
+
+def create_spec_sheet(**kwargs):
+    if 'm' in kwargs:
+        m = kwargs['m']
+    elif 'f' in kwargs:
+        f = kwargs['f']
+    elif 'obj ht' in kwargs:
+        obj_ht = kwargs('obj ht')
+    elif 'obj ang' in kwargs:
+        obj_ang = kwargs('obj ang')        
+    elif 'img ht' in kwargs:
+        img_ht = kwargs('img ht')
+    elif 'epd' in kwargs:
+        epd = kwargs('epd')
+    elif 'fno' in kwargs:
+        fno = kwargs('fno')
+    elif 'nao' in kwargs:
+        nao = kwargs('nao')
+    elif 'na' in kwargs:
+        na = kwargs('na')
+
+    else:
+        return None
+
+
+def do_aperture_spec_to_efl(aper1, aper2):
+    n_0 = 1.0
+    n_k = 1.0
+    if aper1[0] == PupilType.EPD:
+        epd = aper1[1]
+        if aper2[0] == PupilType.FNO:
+            fno = aper2[1]
+            efl = epd * fno
+        if aper2[0] == PupilType.NA:
+            na = aper2[1]
+            slpk = na2slp(na, n=n_k)
+            efl = (epd/2.0)/slpk
 #        if aper1[0] == PupilType.NAO:
 #            nao = aper1[1]
 #            slp0 = n_0*math.tan(math.asin(pupil.value/n_0))
@@ -226,24 +253,25 @@ class SpecSheet:
 #                na = aper2[1]
 #                slpk = n_k*math.tan(math.asin(na/n_k))
 #                efl = (epd/2.0)/slpk
-        if aper1[0] == PupilType.FNO:
-            fno = aper1[1]
-            if aper2[0] == PupilType.EPD:
-                epd = aper2[1]
-                efl = epd * fno
-        if aper1[0] == PupilType.NA:
-            na = aper1[1]
-            slpk = na2slp(na, n=n_k)
-            if aper2[0] == PupilType.EPD:
-                epd = aper2[1]
-                efl = (epd/2.0)/slpk
-        return efl
+    if aper1[0] == PupilType.FNO:
+        fno = aper1[1]
+        if aper2[0] == PupilType.EPD:
+            epd = aper2[1]
+            efl = epd * fno
+    if aper1[0] == PupilType.NA:
+        na = aper1[1]
+        slpk = na2slp(na, n=n_k)
+        if aper2[0] == PupilType.EPD:
+            epd = aper2[1]
+            efl = (epd/2.0)/slpk
+    return efl
 
-    def do_aperture_spec_to_mag(aper1, aper2):
-        if aper1[0].value > aper2[0].value:
-            aper1, aper2 = aper2, aper1
-        n_0 = 1.0
-        n_k = 1.0
+
+def do_aperture_spec_to_mag(aper1, aper2):
+    if aper1[0].value > aper2[0].value:
+        aper1, aper2 = aper2, aper1
+    n_0 = 1.0
+    n_k = 1.0
 #        if aper1[0] == PupilType.EPD:
 #            epd = aper1[1]
 #            if aper2[0] == PupilType.FNO:
@@ -253,65 +281,67 @@ class SpecSheet:
 #                na = aper2[1]
 #                slpk = na2slp(na, n=n_k)
 #                efl = (epd/2.0)/slpk
-        if aper1[0] == PupilType.NAO:
-            nao = aper1[1]
-            slp0 = na2slp(nao, n=n_0)
-            if aper2[0] == PupilType.FNO:
-                fno = aper2[1]
-                slpk = -1/(2*fno)
-                mag = slp0/slpk
-            if aper2[0] == PupilType.NA:
-                na = aper2[1]
-                mag = (nao/na)*(n_k/n_0)
-        return mag
+    if aper1[0] == PupilType.NAO:
+        nao = aper1[1]
+        slp0 = na2slp(nao, n=n_0)
+        if aper2[0] == PupilType.FNO:
+            fno = aper2[1]
+            slpk = -1/(2*fno)
+            mag = slp0/slpk
+        if aper2[0] == PupilType.NA:
+            na = aper2[1]
+            mag = (nao/na)*(n_k/n_0)
+    return mag
 
-    def get_aperture_spec_from_mag(imager, aper_in, aper_out):
-        n_0 = 1.0
-        n_k = 1.0
-        if aper_in[0] == PupilType.EPD:
-            epd = aper_in[1]
-            if aper_out == PupilType.FNO:
-                fno = imager.f/epd
-                aper = (PupilType.FNO, fno)
-            if aper_out == PupilType.NA:
-                slpk = -(epd/2.0)/imager.f
-                na = slp2na(slpk, n=n_k)
-                aper = (PupilType.NA, na)
-        if aper_in[0] == PupilType.NAO:
-            nao = aper_in[1]
-            slp0 = na2slp(nao, n=n_0)
-            if aper_out == PupilType.FNO:
-                slpk = imager.m / slp0
-                fno = -1/(2.0*slpk)
-                aper = (PupilType.FNO, fno)
-            if aper_out == PupilType.NA:
-                na = imager.m * nao
-                aper = (PupilType.NA, na)
-        if aper_in[0] == PupilType.FNO:
-            fno = aper_in[1]
-            if aper_out == PupilType.EPD:
-                epd = imager.f/fno
-                aper = (PupilType.EPD, epd)
-        if aper_in[0] == PupilType.NA:
-            na = aper_in[1]
-            slpk = na2slp(na, n=n_k)
-            if aper_out == PupilType.EPD:
-                epd = 2*slpk*imager.f
-                aper = (PupilType.EPD, epd)
-        return aper
 
-    def get_aperture_spec_from_efl(efl, aper1, aper2):
-        n_0 = 1.0
-        n_k = 1.0
-        if aper1[0] == PupilType.EPD:
-            epd = aper1[1]
-            if aper2[0] == PupilType.FNO:
-                fno = efl/epd
-                aper = (PupilType.FNO, fno)
-            if aper2[0] == PupilType.NA:
-                slpk = -(epd/2.0)/efl
-                na = slp2na(slpk, n=n_k)
-                aper = (PupilType.NA, na)
+def get_aperture_spec_from_mag(imager, aper_in, aper_out):
+    n_0 = 1.0
+    n_k = 1.0
+    if aper_in[0] == PupilType.EPD:
+        epd = aper_in[1]
+        if aper_out == PupilType.FNO:
+            fno = imager.f/epd
+            aper = (PupilType.FNO, fno)
+        if aper_out == PupilType.NA:
+            slpk = -(epd/2.0)/imager.f
+            na = slp2na(slpk, n=n_k)
+            aper = (PupilType.NA, na)
+    if aper_in[0] == PupilType.NAO:
+        nao = aper_in[1]
+        slp0 = na2slp(nao, n=n_0)
+        if aper_out == PupilType.FNO:
+            slpk = imager.m / slp0
+            fno = -1/(2.0*slpk)
+            aper = (PupilType.FNO, fno)
+        if aper_out == PupilType.NA:
+            na = imager.m * nao
+            aper = (PupilType.NA, na)
+    if aper_in[0] == PupilType.FNO:
+        fno = aper_in[1]
+        if aper_out == PupilType.EPD:
+            epd = imager.f/fno
+            aper = (PupilType.EPD, epd)
+    if aper_in[0] == PupilType.NA:
+        na = aper_in[1]
+        slpk = na2slp(na, n=n_k)
+        if aper_out == PupilType.EPD:
+            epd = 2*slpk*imager.f
+            aper = (PupilType.EPD, epd)
+    return aper
+
+
+def get_aperture_spec_from_efl(efl, aper1, aper2):
+    n_0 = 1.0
+    n_k = 1.0
+    if aper1[0] == PupilType.EPD:
+        epd = aper1[1]
+        if aper2[0] == PupilType.FNO:
+            fno = efl/epd
+            aper = (PupilType.FNO, fno)
+        if aper2[0] == PupilType.NA:
+            slpk = -(epd/2.0)/efl
+            na = slp2na(slpk, n=n_k)
+            aper = (PupilType.NA, na)
 #        if aper1[0] == PupilType.NAO:
 #            nao = aper1[1]
 #            slp0 = n_0*math.tan(math.asin(nao/n_0))
@@ -323,50 +353,52 @@ class SpecSheet:
 #                na = aper2[1]
 #                slpk = n_k*math.tan(math.asin(na/n_k))
 #                efl = (epd/2.0)/slpk
-        if aper1[0] == PupilType.FNO:
-            fno = aper1[1]
-            if aper2[0] == PupilType.EPD:
-                epd = efl/fno
-                aper = (PupilType.EPD, epd)
-        if aper1[0] == PupilType.NA:
-            na = aper1[1]
-            slpk = na2slp(na, n=n_k)
-            if aper2[0] == PupilType.EPD:
-                epd = 2*slpk*efl
-                aper = (PupilType.EPD, epd)
-        return aper
-
-    def do_field_spec_to_efl(fld1, fld2):
-        if fld1[0].value > fld2[0].value:
-            fld1, fld2 = fld2, fld1
-        if fld1[0] == FieldType.OBJ_ANG:
-            obj_slp = ang2slp(fld1[1])
-            if fld1[0] == FieldType.IMG_HT:
-                img_ht = fld2[1]
-                efl = img_ht/obj_slp
-        return efl
-
-    def do_field_spec_to_mag(fld1, fld2):
-        if fld1[0].value > fld2[0].value:
-            fld1, fld2 = fld2, fld1
-        if fld1[0] == FieldType.OBJ_HT:
-            obj_ht = fld1[1]
-            if fld1[0] == FieldType.IMG_HT:
-                img_ht = fld2[1]
-                mag = img_ht/obj_ht
-        elif fld1[0] == FieldType.OBJ_ANG:
-            obj_slp = ang2slp(fld1[1])
-            if fld2[0] == FieldType.IMG_ANG:
-                img_slp = ang2slp(fld2[1])
-                mag = obj_slp/img_slp
-        return mag
+    if aper1[0] == PupilType.FNO:
+        fno = aper1[1]
+        if aper2[0] == PupilType.EPD:
+            epd = efl/fno
+            aper = (PupilType.EPD, epd)
+    if aper1[0] == PupilType.NA:
+        na = aper1[1]
+        slpk = na2slp(na, n=n_k)
+        if aper2[0] == PupilType.EPD:
+            epd = 2*slpk*efl
+            aper = (PupilType.EPD, epd)
+    return aper
 
 
-dispatch = {
-  (PupilType.EPD, PupilType.FNO): SpecSheet.do_aperture_spec_to_efl,
-  (PupilType.EPD, PupilType.NA): SpecSheet.do_aperture_spec_to_efl,
-  (PupilType.NAO, PupilType.FNO): SpecSheet.do_aperture_spec_to_efl,
-  (PupilType.NAO, PupilType.NA): SpecSheet.do_aperture_spec_to_efl,
-  (FieldType.OBJ_ANG, FieldType.IMG_HT): SpecSheet.do_field_spec_to_efl,
-  (FieldType.OBJ_HT, FieldType.IMG_HT): SpecSheet.do_field_spec_to_efl,
-  }
+def do_field_spec_to_efl(fld1, fld2):
+    if fld1[0].value > fld2[0].value:
+        fld1, fld2 = fld2, fld1
+    if fld1[0] == FieldType.OBJ_ANG:
+        obj_slp = ang2slp(fld1[1])
+        if fld1[0] == FieldType.IMG_HT:
+            img_ht = fld2[1]
+            efl = img_ht/obj_slp
+    return efl
+
+
+def do_field_spec_to_mag(fld1, fld2):
+    if fld1[0].value > fld2[0].value:
+        fld1, fld2 = fld2, fld1
+    if fld1[0] == FieldType.OBJ_HT:
+        obj_ht = fld1[1]
+        if fld1[0] == FieldType.IMG_HT:
+            img_ht = fld2[1]
+            mag = img_ht/obj_ht
+    elif fld1[0] == FieldType.OBJ_ANG:
+        obj_slp = ang2slp(fld1[1])
+        if fld2[0] == FieldType.IMG_ANG:
+            img_slp = ang2slp(fld2[1])
+            mag = obj_slp/img_slp
+    return mag
+
+
+#dispatch = {
+#  (PupilType.EPD, PupilType.FNO): SpecSheet.do_aperture_spec_to_efl,
+#  (PupilType.EPD, PupilType.NA): SpecSheet.do_aperture_spec_to_efl,
+#  (PupilType.NAO, PupilType.FNO): SpecSheet.do_aperture_spec_to_efl,
+#  (PupilType.NAO, PupilType.NA): SpecSheet.do_aperture_spec_to_efl,
+#  (FieldType.OBJ_ANG, FieldType.IMG_HT): SpecSheet.do_field_spec_to_efl,
+#  (FieldType.OBJ_HT, FieldType.IMG_HT): SpecSheet.do_field_spec_to_efl,
+#  }
---
+++ src/rayoptics/qtgui/idealimagerdialog.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright © 2019 Michael J. Hayford
+""" Qt5 dialog box for ideal imager ui
+
+.. Created on Tue Jun 18 13:09:19 2019
+
+.. codeauthor: Michael J. Hayford
+"""
+
+import math
+
+from rayoptics.optical import specsheet
+
+from PyQt5.QtCore import Qt as qt
+from PyQt5.QtWidgets import (QApplication, QDialog, QRadioButton,
+                             QStackedWidget, QFormLayout, QGridLayout,
+                             QGroupBox, QHBoxLayout, QLabel, QLineEdit,
+                             QPushButton, QCheckBox, QVBoxLayout)
+
+
+class IdealImagerDialog(QDialog):
+    NumGridRows = 3
+    NumButtons = 4
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.dlog_attrs = {}
+
+        self.conjugate_type = 'infinite'
+        self.conjugate_box = self.createConjugateBox(itype=self.conjugate_type)
+
+        # setup finite conjugate defaults
+        self.imager_stack = {}
+        figb = ImagerSpecGroupBox()
+        self.imager_stack['finite'] = figb
+
+        # setup infinite conjugate defaults
+        enabled_list = [False, False, False, False, True]
+        chkbox_enabled_list = [False]*5
+        imager_inputs = {'s': -math.inf, 'f': None}
+        imager = specsheet.IdealImager(None, -math.inf, None, None, None)
+        iigb = ImagerSpecGroupBox(enabled_list=enabled_list, imager=imager,
+                                  chkbox_enabled_list=chkbox_enabled_list,
+                                  imager_inputs=imager_inputs)
+        self.imager_stack['infinite'] = iigb
+
+        self.imager_groupbox_stack = QStackedWidget()
+        self.imager_groupbox_stack.addWidget(iigb)
+        self.imager_groupbox_stack.addWidget(figb)
+
+        imager_groupbox = self.imager_stack[self.conjugate_type]
+        self.imager_groupbox_stack.setCurrentWidget(imager_groupbox)
+
+        mainLayout = QHBoxLayout()
+        mainLayout.addWidget(self.conjugate_box)
+        mainLayout.addWidget(self.imager_groupbox_stack)
+        self.setLayout(mainLayout)
+
+        self.setWindowTitle("Optical Spec Sheet")
+
+    def get_imager(self):
+        imager_groupbox = self.imager_stack[self.conjugate_type]
+        return imager_groupbox.imager, imager_groupbox.imager_inputs
+
+    def createConjugateBox(self, itype='infinite'):
+        conjugate_box = QGroupBox("Conjugates")
+        layout = QVBoxLayout()
+
+        infinite = QRadioButton('Infinite')
+        self.dlog_attrs['infinite'] = infinite
+        layout.addWidget(infinite)
+        infinite.clicked.connect(lambda: self.change_conjugate('infinite'))
+
+        finite = QRadioButton('Finite')
+        self.dlog_attrs['finite'] = finite
+        layout.addWidget(finite)
+        finite.clicked.connect(lambda: self.change_conjugate('finite'))
+
+        conjugate_box.setLayout(layout)
+
+        self.dlog_attrs[itype].setChecked(True)
+
+        return conjugate_box
+
+    def change_conjugate(self, conj_type):
+        print("change_conjugate:", conj_type)
+        if self.conjugate_type is not conj_type:
+            self.update_conjugate(conj_type)
+
+    def update_conjugate(self, conj_type):
+#        print(conj_type)
+        prev = self.imager_groupbox_stack.currentWidget()
+        self.conjugate_type = conj_type
+        new = self.imager_stack[conj_type]
+        if prev.imager is not None:
+            if new.imager is None:
+                new.imager = prev.imager
+            else:
+                prev_enabled_list, _ = prev.get_enabled_lists()
+                new_enabled_list, _ = new.get_enabled_lists()
+                new_imager = list(new.imager)
+                for i, p in enumerate(prev.imager):
+#                    print(i, p, prev_enabled_list[i], new_enabled_list[i])
+                    if (prev_enabled_list[i] and new_enabled_list[i]):
+                        new_imager[i] = p
+                        key = prev.keys[i]
+                        if key in prev.imager_inputs:
+                            new.chkbox_change(qt.Checked, key)
+                            new.imager_inputs[key] = p
+                new.imager = specsheet.IdealImager(*new_imager)
+        self.imager_groupbox_stack.setCurrentWidget(new)
+        new.update_values(new_imager)
+
+
+class ImagerSpecGroupBox(QGroupBox):
+    def __init__(self, keys=None, enabled_list=None, imager=None,
+                 imager_inputs=None, chkbox_enabled_list=None, labels=None,
+                 **kwargs):
+        super().__init__(title='Imager specs', **kwargs)
+        self.dlog_attrs = {}
+
+        self.keys = keys if keys else specsheet.ideal_imager_keys
+        self.labels = labels if labels else specsheet.ideal_imager_labels
+
+        if imager is None:
+            imager = specsheet.IdealImager(None, None, None, None, None)
+        self.imager = imager
+
+        self.imager_inputs = imager_inputs if imager_inputs else {}
+
+        self.enabled_list = enabled_list
+        self.chkbox_enabled_list = chkbox_enabled_list
+        enabled_list, chkbox_enabled_list = self.get_enabled_lists()
+
+        layout = QGridLayout()
+        for i, key in enumerate(self.keys):
+            label = QLabel(self.labels[i]+':')
+            lineEdit = QLineEdit()
+            checkBox = QCheckBox()
+            row = i + 1
+            layout.addWidget(label, row, 0)
+            layout.addWidget(lineEdit, row, 1)
+            layout.addWidget(checkBox, row, 2)
+            self.dlog_attrs[key] = (label, lineEdit, checkBox)
+            lineEdit.setText(ImagerSpecGroupBox.value_to_text(imager[i]))
+            lineEdit.setEnabled(enabled_list[i])
+            # Use returnPressed signal rather than editingFinished. The latter
+            #  fires on change of focus as well as return. We only want a 
+            #  signal when the lineEdit contents change and are committed by
+            #  the user.
+            lineEdit.returnPressed.connect(lambda k=key: self.value_change(k))
+
+            if key in self.imager_inputs:
+                if not checkBox.isChecked():
+                    checkBox.setCheckState(qt.Checked)
+            checkBox.setEnabled(chkbox_enabled_list[i])
+            checkBox.stateChanged.connect(lambda state, k=key:
+                                          self.chkbox_change(state, k))
+
+        self.setLayout(layout)
+
+    def value_change(self, imager_key):
+        label, lineEdit, checkBox = self.dlog_attrs[imager_key]
+#        print(" value_change: {} {} '{}'".format(str(self.imager_inputs),
+#              imager_key, lineEdit.text()))
+        try:
+            value = float(lineEdit.text())
+        except ValueError:
+            return
+        else:
+            if imager_key in self.imager_inputs or len(self.imager_inputs) < 2:
+                self.imager_inputs[imager_key] = value
+                if not checkBox.isChecked():
+                    checkBox.setCheckState(qt.Checked)
+            self.update_values()
+
+    def update_values(self, values=None, enabled_list=None):
+#        print('update_values:', self.imager_inputs)
+        if values is None:
+            inputs = {k: v for (k, v) in self.imager_inputs.items()
+                      if v is not None}
+            imager = specsheet.ideal_imager_setup(**inputs)
+            if imager is not None:
+                self.imager = imager
+
+            if self.imager:
+                value_list = self.imager
+            else:
+                value_list = [None]*len(self.keys)
+        else:
+#            print('update_values: values', values)
+            value_list = values
+
+        if self.imager is not None:
+            # refill gui widgets and set enabled state
+            for i, key in enumerate(self.keys):
+                value = value_list[i]
+                label, lineEdit, checkBox = self.dlog_attrs[key]
+                lineEdit.setText(ImagerSpecGroupBox.value_to_text(value))
+
+        self.update_checkboxes()
+
+    def value_to_text(value):
+        if value is None:
+            value_text = ''
+        elif type(value) is str:
+            value_text = value
+        else:
+            value_text = "{:> #.5f}".format(value)
+        return value_text
+
+    def chkbox_change(self, state, imager_key):
+        label, lineEdit, checkBox = self.dlog_attrs[imager_key]
+        checked = state == qt.Checked
+#        print("chkbox_change:", self.imager_inputs, imager_key, checked)
+        if checked:
+            try:
+                value = float(lineEdit.text())
+            except ValueError:
+                value = None
+            finally:
+                self.imager_inputs[imager_key] = value
+                if not checkBox.isChecked():
+                    checkBox.setChecked(True)
+        else:
+            if imager_key in self.imager_inputs:
+                del self.imager_inputs[imager_key]
+                if checkBox.isChecked():
+                    checkBox.setChecked(False)
+        self.update_checkboxes()
+#        print("  chkbox_exit:", self.imager_inputs, imager_key, checked)
+
+    def get_enabled_lists(self):
+        if self.enabled_list is not None:
+            enabled_list = self.enabled_list
+        else:
+            if len(self.imager_inputs) < 2:
+                enabled_list = [True]*5
+            else:
+                enabled_list = [False]*5
+            for key in self.imager_inputs:
+                enabled_list[self.keys.index(key)] = True
+
+        if self.chkbox_enabled_list is not None:
+            chkbox_enabled_list = self.chkbox_enabled_list
+        else:
+            chkbox_enabled_list = enabled_list
+
+        return enabled_list, chkbox_enabled_list
+
+    def update_checkboxes(self):
+        enabled_list, chkbox_enabled_list = self.get_enabled_lists()
+
+        for i, key in enumerate(self.keys):
+            label, lineEdit, checkBox = self.dlog_attrs[key]
+
+            lineEdit.setEnabled(enabled_list[i])
+            checkBox.setEnabled(chkbox_enabled_list[i])
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    app = QApplication(sys.argv)
+    dialog = IdealImagerDialog()
+    dialog.exec()
