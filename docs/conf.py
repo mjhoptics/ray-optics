786ae847b876e0a96cd99cae2d4c80d738c2cac9
Mike Hayford <mjhoptics@gmail.com>
2019-07-14 18:46:48 -0700

    try to fix ReadTheDocs build issue related to being unable to import external modules

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,20 @@ __location__ = os.path.join(os.getcwd(), os.path.dirname(
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.join(__location__, '../src'))
 
+
+# mock out modules not needed for doc
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = [
+    'ipywidgets',
+]
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # -- Run sphinx-apidoc ------------------------------------------------------
 # This hack is necessary since RTD does not issue `sphinx-apidoc` before running
 # `sphinx-build -b html . _build/html`. See Issue:
