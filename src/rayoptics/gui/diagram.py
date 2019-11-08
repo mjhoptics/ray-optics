0279085cb1246f304e3aaace68ecddd6ebc21ba2
Mike Hayford <mjhoptics@gmail.com>
2019-11-07 16:27:33 -0800

    add wedge and slide constraints for yybar point editing

--- src/rayoptics/gui/diagram.py
+++ src/rayoptics/gui/diagram.py
@@ -7,6 +7,7 @@
 .. codeauthor: Michael J. Hayford
 """
 
+import math
 import numpy as np
 
 from rayoptics.gui.util import GUIHandle, bbox_from_poly
@@ -15,6 +16,8 @@ from rayoptics.optical.elements import remove_ifc_gp_ele
 from rayoptics.optical.model_constants import ht, slp
 from rayoptics.optical.model_constants import pwr, tau, indx, rmd
 from rayoptics.util.rgb2mpl import rgb2mpl
+from rayoptics.util.misc_math import projected_point_on_radial_line
+from rayoptics.util import misc_math
 
 
 class Diagram():
@@ -100,7 +103,44 @@ class Diagram():
         shape = []
         for x, y in zip(parax_model.pr, parax_model.ax):
             shape.append([x[self.type_sel], y[self.type_sel]])
+        shape = np.array(shape)
         return shape
+
+
+def compute_slide_line(shape, node, imode):
+    """ compute a constraint line to keep the overall length of the airspaces
+        surrounding `node` constant
+    """
+    def distance_sqr(pt):
+        return pt[0]**2 + pt[1]**2
+    num_nodes = len(shape)
+    if node > 0 and node < num_nodes-1:
+        pt0 = shape[node-1]
+        pt1 = shape[node]
+        pt2 = shape[node+1]
+        if imode == 'transmit':
+            # if transmitting, constrain movement of node to a line parallel
+            #  to the line between the previous and next nodes.
+            origin2line = misc_math.perpendicular_from_origin(pt0, pt2)
+            pt2line = misc_math.perpendicular_to_line(pt1, pt0, pt2)
+            scale_factor = (origin2line - pt2line)/origin2line
+            new_pt0 = scale_factor*pt0
+            new_pt2 = scale_factor*pt2
+            return new_pt0, new_pt2
+        elif imode == 'reflect':
+            # if reflecting, constrain movement to a radial line from the
+            #  origin through the selected node
+            origin = np.array([0., 0.])
+            dist_pt0 = distance_sqr(pt0)
+            dist_pt1 = distance_sqr(pt1)
+            dist_pt2 = distance_sqr(pt2)
+            # figure out how long a line to draw
+            dist = dist_pt0 if dist_pt0 > dist_pt1 else dist_pt1
+            dist = dist_pt2 if dist_pt2 > dist else dist
+            scale_factor = math.sqrt(dist/dist_pt1)
+            new_pt1 = scale_factor*pt1
+            return origin, new_pt1
+    return None
 
 
 class DiagramNode():
@@ -114,22 +154,26 @@ class DiagramNode():
 
     def update_shape(self, view):
         n_color = rgb2mpl([138, 43, 226])  # blueviolet
+        sys = self.diagram.opt_model.parax_model.sys
         shape = self.diagram.shape
-        num_nodes = len(shape)
         self.handles['shape'] = shape[self.node], 'vertex', {'linestyle': '',
                                                              'marker': 's',
                                                              'picker': 6,
                                                              'color': n_color,
                                                              'hilite': 'red',
                                                              'zorder': 3.}
-        if self.node > 1 and self.node < num_nodes-1:
-            pt0 = shape[self.node-1]
-            pt2 = shape[self.node+1]
-            seg = [pt0, pt2]
-            self.handles['dist'] = seg, 'polyline', {'picker': 6,
-                                                     'color': n_color,
-                                                     'hilite': n_color,
-                                                     'zorder': 1.}
+        # define the "constant spacing" or "slide" constraint
+        slide_pts = compute_slide_line(shape, self.node, sys[self.node][rmd])
+        if slide_pts is not None:
+            seg = [*slide_pts]
+            f_color = rgb2mpl([138, 43, 226, 63])  # blueviolet
+            h_color = rgb2mpl([138, 43, 226, 255])  # blueviolet
+
+            self.handles['slide'] = seg, 'polyline', {'linestyle': ':',
+                                                      'picker': 6,
+                                                      'color': f_color,
+                                                      'hilite': h_color,
+                                                      'zorder': 2.5}
         gui_handles = {}
         for key, graphics_handle in self.handles.items():
             poly_data, poly_type, kwargs = graphics_handle
@@ -159,10 +203,25 @@ class DiagramNode():
     def get_label(self):
         return 'node' + str(self.node)
 
+    def constrain_to_line_action(pt0, pt2):
+
+        def constrain_to_line(input_pt):
+            """ constrain the input point to the line pt0 to pt2 """
+            output_pt = misc_math.projected_point_on_line(input_pt, pt0, pt2)
+            return output_pt
+
+        return constrain_to_line
+
     def handle_actions(self):
         actions = {}
         actions['shape'] = EditNodeAction(self)
-        actions['dist'] = EditNodeAction(self)
+        slide_filter = None
+        sys = self.diagram.opt_model.parax_model.sys
+        slide_pts = compute_slide_line(self.diagram.shape, self.node,
+                                       sys[self.node][rmd])
+        if slide_pts is not None:
+            slide_filter = DiagramNode.constrain_to_line_action(*slide_pts)
+        actions['slide'] = EditNodeAction(self, filter=slide_filter)
         return actions
 
 
@@ -179,22 +238,22 @@ class DiagramEdge():
         shape = self.diagram.shape
         edge_poly = shape[self.node:self.node+2]
         self.handles['shape'] = edge_poly, 'polyline', {'picker': 6,
-                                                        'hilite': 'red'}
+                                                        'hilite': 'red',
+                                                        'zorder': 2.}
         area_poly = [[0, 0]]
         area_poly.extend(edge_poly)
         fill_color = self.render_color()
-        self.handles['area'] = area_poly, 'polygon', {'fill_color': fill_color}
+        self.handles['area'] = area_poly, 'polygon', {'fill_color': fill_color,
+                                                      'zorder': 1.}
 
         gui_handles = {}
         for key, graphics_handle in self.handles.items():
             poly_data, poly_type, kwargs = graphics_handle
             poly = np.array(poly_data)
             if poly_type == 'polygon':
-                p = view.create_polygon(poly, self.render_color(),
-                                        zorder=2.5, **kwargs)
+                p = view.create_polygon(poly, self.render_color(), **kwargs)
             elif poly_type == 'polyline':
-                priority = 2.
-                p = view.create_polyline(poly, zorder=priority, **kwargs)
+                p = view.create_polyline(poly, **kwargs)
             else:
                 break
             gui_handles[key] = GUIHandle(p, bbox_from_poly(poly))
@@ -220,23 +279,77 @@ class DiagramEdge():
 
 class EditNodeAction():
     """ Action to move a diagram node, using an input pt """
-    def __init__(self, dgm_node):
+    def __init__(self, dgm_node, filter=None):
         diagram = dgm_node.diagram
         self.cur_node = None
+        self.pt0 = None
+        self.pt2 = None
+        self.filter = filter
+
+        def point_on_line(pt1, pt2, t):
+            d = pt2 - pt1
+            return pt1 + t*d
+
+        def constrain_to_wedge(input_pt):
+            """ keep the input point inside the wedge of adjacent points """
+
+            if self.pt0 is not None:
+                x_prod0 = input_pt[0]*self.pt0[1] - self.pt0[0]*input_pt[1]
+                if x_prod0 < 0:
+                    # pin to boundary
+                    output_pt = projected_point_on_radial_line(input_pt,
+                                                               self.pt0)
+                    return output_pt
+
+            if self.pt2 is not None:
+                x_prod2 = input_pt[0]*self.pt2[1] - self.pt2[0]*input_pt[1]
+                if x_prod2 > 0:
+                    # pin to boundary
+                    output_pt = projected_point_on_radial_line(input_pt,
+                                                               self.pt2)
+                    return output_pt
+
+            return input_pt
 
         def on_select(fig, event):
+            nonlocal diagram
+            # we don't allow points to move onto their adjacent neighbors. Use
+            #  a buffer amount when constraining to the wedge
+            buffer_fraction = 0.0025
             self.cur_node = dgm_node.node
+            pt1 = diagram.shape[self.cur_node]
+            if self.cur_node == 0:
+                pt2 = diagram.shape[self.cur_node+1]
+                self.pt2 = point_on_line(pt1, pt2, 1-buffer_fraction)
+                self.pt0 = None
+            elif self.cur_node == len(diagram.shape)-1:
+                pt0 = diagram.shape[self.cur_node-1]
+                self.pt0 = point_on_line(pt0, pt1, buffer_fraction)
+                self.pt2 = None
+            else:
+                pt0 = diagram.shape[self.cur_node-1]
+                self.pt0 = point_on_line(pt0, pt1, buffer_fraction)
+                pt2 = diagram.shape[self.cur_node+1]
+                self.pt2 = point_on_line(pt1, pt2, 1-buffer_fraction)
 
         def on_edit(fig, event):
-            event_data = np.array([event.xdata, event.ydata])
-            diagram.apply_data(self.cur_node, event_data)
-            fig.refresh_gui()
+            if event.xdata is not None and event.ydata is not None:
+                event_data = np.array([event.xdata, event.ydata])
+                if self.filter:
+                    event_data = self.filter(event_data)
+                event_data = constrain_to_wedge(event_data)
+                diagram.apply_data(self.cur_node, event_data)
+                fig.refresh_gui()
 
         def on_release(fig, event):
-            event_data = np.array([event.xdata, event.ydata])
-            diagram.apply_data(self.cur_node, event_data)
-            fig.refresh_gui()
-            self.cur_node = None
+            if event.xdata is not None and event.ydata is not None:
+                event_data = np.array([event.xdata, event.ydata])
+                if self.filter:
+                    event_data = self.filter(event_data)
+                event_data = constrain_to_wedge(event_data)
+                diagram.apply_data(self.cur_node, event_data)
+                fig.refresh_gui()
+                self.cur_node = None
 
         self.actions = {}
         self.actions['drag'] = on_edit
--- src/rayoptics/util/misc_math.py
+++ src/rayoptics/util/misc_math.py
@@ -45,9 +45,51 @@ def perpendicular_to_line(pt, pt1, pt2):
     """ return perpendicular distance of pt from the line between pt1 and pt2
     """
     d = perpendicular_distance_2d(pt, pt1, pt2)
-    return (((pt2[0] - pt1[0])*(pt1[1] - pt0[1])
-            - (pt1[0] - pt0[0])*(pt2[1] - pt1[1]))
+    return (((pt2[0] - pt1[0])*(pt1[1] - pt[1])
+            - (pt1[0] - pt[0])*(pt2[1] - pt1[1]))
             / sqrt(distance_sqr_2d(pt2, pt1)))
+
+
+def perpendicular_from_origin(pt1, pt2):
+    """ return perpendicular distance of the origin from the line between
+    pt1 and pt2
+    """
+    return (((pt2[0] - pt1[0])*(pt1[1]) - (pt1[0])*(pt2[1] - pt1[1]))
+            / sqrt(distance_sqr_2d(pt2, pt1)))
+
+
+def projected_point_on_line(pt, pt1, pt2):
+    e1 = pt2 - pt1
+    e2 = pt - pt1
+    # get dot product of e1, e2
+    dot_prod = np.dot(e1, e2)
+    # get squared length of e1
+    len_sqr_e1 = e1[0]**2 + e1[1]**2
+    p = np.array([(pt1[0] + (dot_prod * e1[0]) / len_sqr_e1),
+                 (pt1[1] + (dot_prod * e1[1]) / len_sqr_e1)])
+    return p
+
+
+def projected_point_on_radial_line(pt, radial_pt):
+    # get dot product of radial_pt, pt
+    dot_prod = np.dot(radial_pt, pt)
+    # get squared length of radial_pt
+    len_sqr = radial_pt[0]**2 + radial_pt[1]**2
+    p = np.array([(dot_prod * radial_pt[0]) / len_sqr,
+                 (dot_prod * radial_pt[1]) / len_sqr])
+    return p
+
+
+def projected_point_on_radial_line_full(pt, radial_pt):
+    x_prod = pt[0]*radial_pt[1] - radial_pt[0]*pt[1]
+    # get dot product of radial_pt, pt
+    dot_prod = np.dot(radial_pt, pt)
+    # get squared length of radial_pt
+    len_sqr = radial_pt[0]**2 + radial_pt[1]**2
+    p = np.array([(dot_prod * radial_pt[0]) / len_sqr,
+                 (dot_prod * radial_pt[1]) / len_sqr])
+    dist = x_prod/sqrt(len_sqr)
+    return p, dist
 
 
 def euler2opt(e):
