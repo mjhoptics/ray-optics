3f16990c3081633a3002f7dfbf9fd3cec1cb2229
Mike Hayford <mjhoptics@gmail.com>
2022-07-28 15:02:35 -0700

    add versioneer recommended args to setup() arg list

--- setup.py
+++ setup.py
@@ -22,4 +22,6 @@ except VersionConflict:
 
 
 if __name__ == "__main__":
-    setup(use_pyscaffold=True)
+    setup(use_pyscaffold=True,
+          version=versioneer.get_version(),
+          cmdclass=versioneer.get_cmdclass())
