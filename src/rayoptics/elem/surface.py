f37316c3452f7c9fa3ff221f0747ebad74967c7c
Mike Hayford <mjhoptics@gmail.com>
2022-12-04 21:02:51 -0800

    better support of listobj for thinlens and clear apertures.

--- src/rayoptics/elem/surface.py
+++ src/rayoptics/elem/surface.py
@@ -382,7 +382,7 @@ class Circular(Aperture):
         self.radius = radius
 
     def listobj_str(self):
-        o_str = f"radius={self.radius}\n"
+        o_str = f"ca: radius={self.radius}\n"
         o_str += super().listobj_str()
         return o_str
 
@@ -411,7 +411,7 @@ class Rectangular(Aperture):
         self.y_half_width = y_half_width
 
     def listobj_str(self):
-        o_str = (f"{type(self).__name__}: x_half_width={self.x_half_width}"
+        o_str = (f"ca: {type(self).__name__}: x_half_width={self.x_half_width}"
                  f"   y_half_width={self.y_half_width}\n")
         o_str += super().listobj_str()
         return o_str
@@ -440,7 +440,7 @@ class Elliptical(Aperture):
         self.y_half_width = y_half_width
 
     def listobj_str(self):
-        o_str = (f"{type(self).__name__}: x_half_width={self.x_half_width}"
+        o_str = (f"ca: {type(self).__name__}: x_half_width={self.x_half_width}"
                  f"   y_half_width={self.y_half_width}\n")
         o_str += super().listobj_str()
         return o_str
